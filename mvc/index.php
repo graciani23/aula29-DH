5791c7826d9180144c5f8b227db9a977fe16fb44
graciani23 <gracianigss@gmail.com>
2020-02-19 11:40:41 -0300

    configuração do mcv

--- mvc/index.php
+++ mvc/index.php
@@ -1,8 +1,19 @@
 <?php
     require 'Controller/formController.php';
 
+    //echo $_SERVER['REQUEST_URI'];die;
+
+    if($_SERVER['REQUEST_URI'] === '/graciani/aula29/mvc/formulario') {
+
     $obj = new FormController();
     $obj->form();
 
+    } else if ($_SERVER['REQUEST_URI'] === '/graciani/aula29/mvc/') {
+    $obj = new FormController();
+    $obj->home();    
+    
+    } else {
+        echo "404 - Não encontrado";
+    }
     //echo "Eu quero ser um MVC"
 ?>
